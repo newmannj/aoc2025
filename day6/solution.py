d1bf26f95f96bae42eca9a9fee00e20994565aae
Joseph Newmann <newmann.joseph@gmail.com>
2025-12-07 07:54:29 -0700

    finished day6

--- day6/solution.py
+++ day6/solution.py
@@ -1,5 +1,6 @@
 from collections import defaultdict
 import re
+from functools import reduce
 
 # p1
 columns = defaultdict(list)
@@ -19,26 +20,37 @@ for col in columns.values():
         elif operation == "*":
             acc *= int(r)
     total += acc
-print(total)
+# print(total)
 
 # p2
-columns_2 = []
-with open("test.txt") as file:
+grid = []
+with open("input.txt") as file:
     for line in file:
-        values = re.findall(r"\S+", line)
-        for i, v in enumerate(values):
-            digits = list(v)
-            digits.reverse()
-            print(digits)
+        grid.append(line.replace("\n", ""))
 
+
+x = len(grid[0]) - 1
+problem_inputs: list[int] = []
 total = 0
-for col in columns.values():
-    operation = col.pop(-1)
-    acc = int(col.pop(0))
-    for r in col:
-        if operation == "+":
-            acc += int(r)
-        elif operation == "*":
-            acc *= int(r)
-    total += acc
+while x >= 0:
+    y = 0
+    # Ignore the last row for now
+    col_acc: str = ""
+    while y <= len(grid) - 1:
+        if grid[y][x] in ["+", "*"]:
+            problem_inputs.append(int("".join(col_acc)))
+            if grid[y][x] == "+":
+                total += sum(problem_inputs)
+            else:
+                total += reduce(lambda x, y: x * y, problem_inputs)
+            break
+        else:
+            col_acc += grid[y][x]
+        y += 1
+    result = [x for x in col_acc if x != " "]
+    if len(result) == 0:
+        problem_inputs = []
+    else:
+        problem_inputs.append(int("".join(result)))
+    x -= 1
 print(total)
