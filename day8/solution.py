db2c6dff58786051a4cafc18954d36fa8fca44bc
Joseph Newmann <newmann.joseph@gmail.com>
2025-12-08 22:49:16 -0700

    day8

--- day8/solution.py
+++ day8/solution.py
@@ -2,7 +2,7 @@ import math
 from functools import reduce
 
 coordinates = []
-with open("test.txt") as file:
+with open("input.txt") as file:
     for line in file:
         as_arr = line.strip().split(",")
         coordinates.append(tuple([int(x) for x in as_arr]))
@@ -27,33 +27,37 @@ while i < len(coordinates) - 1:
         j += 1
     i += 1
 distances = sorted(distances, key=lambda x: x[0])
-# Construct a list of dicts with the coordinates of all boxes in circuit
-circuits: list[dict[tuple, bool]] = []
+# Construct a list of sets with the coordinates of all boxes in circuit
+# TODO: Need to merge circuits that may have been connected by something down the line
+circuits: list[set[tuple]] = []
 num_connected = 0
 for d in distances:
-    if num_connected >= 10:
+    if num_connected >= 1000:
         break
     placed = False
-    for c in circuits:
-        # If both are in the circuit, "nothing happened :)"
-        if d[1] in c and d[2] in c:
-            placed = True
-            continue
-        # If either is in the circuit, re-enable both
-        elif d[1] in c or d[2] in c:
-            c[d[1]] = True
-            c[d[2]] = True
-            placed = True
-            num_connected += 1
-            break
-    # If we haven't found a match, append a new circuit
-    if not placed:
-        new = {}
-        new[d[1]] = True
-        new[d[2]] = True
+    # Get the circuit for each box. We assume there will only ever be one
+    possible_d1_circuits = [c for c in circuits if d[1] in c]
+    possible_d2_circuits = [c for c in circuits if d[2] in c]
+    d1_circuit = possible_d1_circuits[0] if len(possible_d1_circuits) != 0 else set()
+    d2_circuit = possible_d2_circuits[0] if len(possible_d2_circuits) != 0 else set()
+    # If there is no circuits for either node, create a new one
+    if not d1_circuit and not d2_circuit:
+        new = {d[1], d[2]}
         circuits.append(new)
         num_connected += 1
-circuit_lengths = sorted([len(c.keys()) for c in circuits], reverse=True)
+    elif difference := d1_circuit ^ d2_circuit:
+        # If there is a difference, remove both circuits, merge to d1, add back
+        circuits = [c for c in circuits if c != d1_circuit and c != d2_circuit]
+        merged = d1_circuit.union(d2_circuit)
+        merged.add(d[1])
+        merged.add(d[2])
+        circuits.append(merged)
+        num_connected += 1
+    else:
+        num_connected += 1
+
+
+circuit_lengths = sorted([len(c) for c in circuits], reverse=True)
 print(circuit_lengths)
 result = reduce(lambda x, y: x * y, circuit_lengths[:3])
 print(result)
