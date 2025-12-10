9950f8974723086b37a79386c4d804f9c5b294ee
Joseph Newmann <newmann.joseph@gmail.com>
2025-12-09 22:22:11 -0700

    better but still not right

--- day9/solution.py
+++ day9/solution.py
@@ -3,44 +3,70 @@ green_tiles = []
 
 
 def print_grid(red_tiles, green_tiles):
-    grid_height = 15
-    grid_width = 15
+    grid_height = 100_000
+    grid_width = 100_000
     grid = [["." for x in range(0, grid_width)] for y in range(0, grid_height)]
     for t in red_tiles:
-        grid[t[0]][t[1]] = "#"
+        grid[t[1]][t[0]] = "#"
     for t in green_tiles:
-        grid[t[0]][t[1]] = "X"
+        grid[t[1]][t[0]] = "X"
     print("-" * grid_width)
     for row in grid:
         print(row)
-    print("-" * grid_width)
 
 
-with open("test.txt") as file:
+# UP, RIGHT, DOWN, LEFT
+directions = [[0, 1], [1, 0], [0, -1], [-1, 0]]
+
+with open("input.txt") as file:
     for r, line in enumerate(file):
         next_tile = tuple([int(v) for v in line.strip().split(",")])
-        # Starting at second tile location, calculate green tiles
-        if len(red_tiles) > 0:
-            prev_tile = red_tiles[r - 1]
-            # If y is aligned, shift x otherwise shift y
-            index_to_shift = 0 if prev_tile[1] == next_tile[1] else 1
-            aligned_index = 0 if index_to_shift == 1 else 1
-            step = -1 if prev_tile[index_to_shift] > next_tile[index_to_shift] else 1
-            # Don't put a green tile where a red tile is
-            for x in range(
-                prev_tile[index_to_shift] + 1, next_tile[index_to_shift], step
-            ):
-                new_tile = [0, 0]
-                new_tile[index_to_shift] = x
-                new_tile[aligned_index] = prev_tile[aligned_index]
-                green_tiles.append(tuple(new_tile))
         red_tiles.append(next_tile)
-        print_grid(red_tiles, green_tiles)
-        if r == 3:
+
+
+i = 0
+while i < len(red_tiles) - 1:
+    prev_tile = red_tiles[i]
+    next_tile = red_tiles[i + 1]
+    x_diff = prev_tile[0] - next_tile[0]
+    y_diff = prev_tile[1] - next_tile[1]
+    if x_diff < 0:
+        direction = directions[1]
+    elif x_diff > 0:
+        direction = directions[3]
+    elif y_diff < 0:
+        direction = directions[0]
+    elif y_diff > 0:
+        direction = directions[2]
+    while True:
+        prev_tile = (prev_tile[0] + direction[0], prev_tile[1] + direction[1])
+        if prev_tile == next_tile:
             break
+        else:
+            green_tiles.append(prev_tile)
+    i += 1
 
 
-# TODO: Connect the last and first tile
+prev_tile = red_tiles[-1]
+next_tile = red_tiles[0]
+x_diff = prev_tile[0] - next_tile[0]
+y_diff = prev_tile[1] - next_tile[1]
+if x_diff < 0:
+    direction = directions[1]
+elif x_diff > 0:
+    direction = directions[3]
+elif y_diff < 0:
+    direction = directions[0]
+elif y_diff > 0:
+    direction = directions[2]
+while True:
+    prev_tile = (prev_tile[0] + direction[0], prev_tile[1] + direction[1])
+    if prev_tile == next_tile:
+        break
+    else:
+        green_tiles.append(prev_tile)
+
+print_grid(red_tiles, green_tiles)
 
 
 def get_area(p1, p2):
