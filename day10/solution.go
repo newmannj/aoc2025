dda44a5aa8ab2a9b82bfa4222c06507c39782213
Joseph Newmann <newmann.joseph@gmail.com>
2025-12-20 07:34:37 -0700

    getting there, got a SO error heck yeah clearly gotta fix end state

--- day10/solution.go
+++ day10/solution.go
@@ -5,12 +5,37 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 	"strings"
 )
+
+func getPressResult(button string, state string) string {
+	// Parse buttons
+	button_list := strings.Split(strings.Trim(button, "()"), ",")
+	for _, b := range button_list {
+		b_index, _ := strconv.Atoi(b)
+		char_at_button := string(state[b_index])
+		var replace_char string
+		if char_at_button == "." {
+			replace_char = "#"
+		} else {
+			replace_char = "."
+		}
+		state = state[:b_index] + string(replace_char) + state[b_index+1:]
+
+	}
+	return state
+}
 
 func bfs(end_state string, cur_state string, buttons []string, last_press string, presses int) int {
 	if strings.Compare(end_state, cur_state) == 0 {
 		return presses
+	}
+	for _, b := range buttons {
+		// Calculate what happens when you press button
+		next_state := getPressResult(b, cur_state)
+		bfs(end_state, next_state, buttons, b, presses+1)
+
 	}
 	// Otherwise, press all buttons except for the one that was pressed
 	// to get into this state.
@@ -24,8 +49,9 @@ func getMinimalPresses(line string) int {
 	splitLine := strings.Split(line, " ")
 	machine = strings.Trim(splitLine[0], "[]")
 	buttons = splitLine[1 : len(splitLine)-1]
-	fmt.Println(machine)
-	fmt.Println(buttons)
+	start_state := strings.Replace(strings.Clone(machine), "#", ".", -1)
+	fmt.Println("getting minimal presses for machine")
+	bfs(machine, start_state, buttons, "", 0)
 
 	return 0
 }
